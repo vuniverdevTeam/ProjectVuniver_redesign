c4975c1aff5f0f0bcdbd20808421829f1294c626
vuniverTeam <gearbots@yandex.com>
2015-02-18 16:03:44 +0200

    Первый коммит

---
+++ landingModal.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Cheese on 18.02.2015.
+ */
+function showModal()
+{
+    document.getElementById('try').style.display="block";
+}
+function hideModal()
+{
+    document.getElementById('try').style.display="none";
+}
